b70dcea8db2656a7212b1df1b1ca0c3668bc7072
Dinyar <dinyar@avaros.ca>
2021-04-12 17:09:56 -0400

    Updated Signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function Signup() {
@@ -58,7 +59,7 @@ export default function Signup() {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? Log In
+        Already have an account? <Link to="/login">Log In</Link>
       </div>
     </>
   );
