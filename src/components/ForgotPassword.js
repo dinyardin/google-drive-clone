0c62d196e2dd37b17faf01f32d0cea7df53820fa
Dinyar <dinyar@avaros.ca>
2021-04-13 11:37:51 -0400

    Added forgot password component with its routings.

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -3,9 +3,8 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export default function Login() {
+export default function ForgotPassword() {
   const emailRef = useRef();
-  const passwordRef = useRef();
   const { login } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -17,7 +16,7 @@ export default function Login() {
     try {
       setError("");
       setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
+      //await login(emailRef.current.value, passwordRef.current.value);
       history.push("/");
     } catch {
       setError("Failed to sign in");
@@ -28,27 +27,23 @@ export default function Login() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
+          <h2 className="text-center mb-4">Password Reset</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
             <Button
               disabled={loading ? true : false}
               className="w-100"
               type="submit"
             >
-              Log In
+              Reset Password
             </Button>
           </Form>
           <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot password ?</Link>
+            <Link to="/login">Log In</Link>
           </div>
         </Card.Body>
       </Card>
