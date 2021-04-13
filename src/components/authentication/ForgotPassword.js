b9ff0586255e107430eb361b813dd16ca614ad99
Dinyar <dinyar@avaros.ca>
2021-04-13 14:14:00 -0400

    Moved authentication related files into its own dedicated authentication folder

--- src/components/authentication/ForgotPassword.js
+++ src/components/authentication/ForgotPassword.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function ForgotPassword() {
   const emailRef = useRef();
