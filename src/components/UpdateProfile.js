65694105c05a805b1e1d590ecd5a2874dcab3d50
Dinyar <dinyar@avaros.ca>
2021-04-13 12:34:23 -0400

    Added update profile functionality

---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,100 @@
+import React, { useState, useRef } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function UpdateProfile() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { currentUser, updatePassword, updateEmail } = useAuth();
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+      //Also dont forget to validate passwords are at least 6 chars long, as otherwise firebase wont allow user creation.
+      //WEAK_PASSWORD : Password should be at least 6 characters (Actual error from firebase)
+    }
+
+    // We can use this seperately if we want to check this below condition
+    // if (emailRef.current.value === currentUser.email) {
+    //   return setError("Email has not been changed");
+    // }
+
+    const promises = [];
+    setError("");
+    setMessage("");
+    setLoading(true);
+    if (emailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(emailRef.current.value));
+    }
+    if (passwordRef.current.value) {
+      promises.push(updatePassword(passwordRef.current.value));
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        setMessage("Profile successfully updated");
+        history.push("/");
+      })
+      .catch(() => {
+        setError("Failed to update account");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Update Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                defaultValue={currentUser.email}
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                placeholder="Leave blank to keep the current password"
+              />
+            </Form.Group>
+            <Form.Group id="passord-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                placeholder="Leave blank to keep the current password"
+              />
+            </Form.Group>
+            <Button
+              disabled={loading ? true : false}
+              className="w-100"
+              type="submit"
+            >
+              Update
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/">Cancel</Link>
+      </div>
+    </>
+  );
+}
